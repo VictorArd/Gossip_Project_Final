068451f80e2e8b3c6f279592b6c941b3d79b3f63
VictorArd <victor.airiaud@outlook.fr>
2019-02-15 19:06:24 +0100

    Add User page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+
   root "accueil#index"
   resources :accueil, only: [:index]
   resources :team, only: [:index]
   resources :contact, only: [:index]
   resources :welcome, only: [:show]
   resources :gossip, only: [:show]
+  resources :user, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
