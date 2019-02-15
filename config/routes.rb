622577efd929c58061f23575beedef8b5d5773ed
VictorArd <victor.airiaud@outlook.fr>
2019-02-15 18:37:42 +0100

    Show Gossip

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :team, only: [:index]
   resources :contact, only: [:index]
   resources :welcome, only: [:show]
+  resources :gossip, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
